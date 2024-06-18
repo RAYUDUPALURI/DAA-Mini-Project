88a0ca03c0a86bc56d6d7023b3f5ff01c8ad535f
SubbaRayudu <152466129+RAYUDUPALURI@users.noreply.github.com>
2024-06-18 09:50:35 +0530

    Delete DAA Mini Projects/range from X and Y directory

--- DAA Mini Projects/range from X and Y/app.py
+++
@@ -1,8 +0,0 @@
-X = int(input("Enter the starting integer (X): "))
-Y = int(input("Enter the ending integer (Y): "))
-
-for number in range(X, Y + 1):
-    print(number)
-
-my_range = list(range(X, Y + 1))
-print(my_range)
